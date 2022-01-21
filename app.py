fb07a6a288cbd2c6d06f6fc46ac595bd7bd9d299
Maksym <mmsli@icloud.com>
2022-01-21 04:32:39 +0200

    fixed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, make_response
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
@@ -75,7 +75,7 @@ api = Api(app)
 
 
 movie_ns = api.namespace('movies')
-director_ns = api.namespace('director')
+director_ns = api.namespace('directors')
 genre_ns = api.namespace('genres')
 
 
@@ -83,21 +83,26 @@ genre_ns = api.namespace('genres')
 class MoviesView(Resource):
     def get(self):
         page = request.args.get("page")
-        if page is None:
-            return "404", 404
-        elif page.isalpha():
-            return "404", 404
+        director_id = request.args.get("director_id")
+        genre_id = request.args.get("genre_id")
+        if page:
+            all_movies = db.session.query(Movie).limit(5).offset((int(page) - 1) * 5).all()
+            return make_response(jsonify(movies_schema.dump(all_movies)), 200)
+        elif director_id:
+            movies = db.session.query(Movie).filter(Movie.director_id == int(director_id)).all()
+            return make_response(jsonify(movies_schema.dump(movies)), 200)
+        elif genre_id:
+            movies = db.session.query(Movie).filter(Movie.genre_id == int(genre_id)).all()
+            return make_response(jsonify(movies_schema.dump(movies)), 200)
         else:
-            if int(page) == 1:
-                all_movies = db.session.query(Movie).limit(5).all()
-                movies_dict = movies_schema.dump(all_movies)
-                return jsonify(movies_dict)
-            elif int(page) > 1:
-                all_movies = db.session.query(Movie).limit(5).offset((int(page) - 1) * 5).all()
-                movies_dict = movies_schema.dump(all_movies)
-                return jsonify(movies_dict)
-            else:
-                return "404", 404
+            return "404", 404
+
+    def post(self):
+        data = request.json
+        new_movie = Movie(**data)
+        with db.session.begin():
+            db.session.add(new_movie)
+        return "done", 201
 
 
 @movie_ns.route("/<int:id>")
@@ -109,30 +114,65 @@ class MovieView(Resource):
         except Exception:
             return "404", 404
 
+    def put(self, id: int):
+        movie = db.session.query(Movie).get(id)
+        data = request.json
+        movie.title = data.get("title")
+        movie.description = data.get("description")
+        movie.trailer = data.get("trailer")
+        movie.year = data.get("year")
+        movie.rating = data.get("rating")
+        movie.genre_id = data.get("genre_id")
+        movie.director_id = data.get("director_id")
+        db.session.add(movie)
+        db.session.commit()
+        return "", 204
 
-@movie_ns.route("/director/")
-class MoviesView(Resource):
-    def get(self):
-        director_id = request.args.get("director_id")
-        if director_id is None:
-            return "404", 404
-        elif director_id.isalpha():
-            return "404", 404
-        else:
-            movies = db.session.query(Movie).filter(Movie.director_id == int(director_id)).all()
-            return jsonify(movies_schema.dump(movies))
+    def delete(self, id: int):
+        movie = db.session.query(Movie).get(id)
+        db.session.delete(movie)
+        db.session.commit()
+        return "", 204
 
-@movie_ns.route("/genre/")
-class MoviesView(Resource):
+
+@director_ns.route("/")
+class DirectorsView(Resource):
     def get(self):
-        genre_id = request.args.get("genre_id")
-        if genre_id is None:
+        all_directors = db.session.query(Director).all()
+        return make_response(jsonify(directors_schema.dump(all_directors)), 200)
+
+
+@director_ns.route("/<int:id>")
+class DirectorView(Resource):
+    def get(self, id: int):
+        try:
+            director = db.session.query(Director).filter(Director.id == id).one()
+            return jsonify(director_schema.dump(director))
+        except Exception:
             return "404", 404
-        elif genre_id.isalpha():
-            return "404", 404
-        else:
-            movies = db.session.query(Movie).filter(Movie.genre_id == int(genre_id)).all()
-            return jsonify(movies_schema.dump(movies))
+
+
+@genre_ns.route("/")
+class GenresView(Resource):
+    def get(self):
+        all_genre = db.session.query(Genre).all()
+        return make_response(jsonify(directors_schema.dump(all_genre)), 200)
+
+
+@genre_ns.route("/<int:id>")
+class GenreView(Resource):
+    def get(self, id: int):
+        try:
+            genre = db.session.query(Genre).filter(Genre.id == id).one()
+            movies = db.session.query(Movie.title).filter(Movie.genre_id == id).all()
+            movies_dict = movies_schema.dump(movies)
+            genre_dict = genre_schema.dump(genre)
+            list_films = [title["title"] for title in movies_dict]
+            genre_dict["movies"] = list_films
+
+            return make_response(jsonify(genre_dict), 200)
+        except Exception:
+            return f"404", 404
 
 
 if __name__ == '__main__':
